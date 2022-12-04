eba0dbf529c89d434e3f3188db22c03f6c350e91
Halit Cetin <halitcetin@live.com>
2022-12-04 14:20:23 +0100

    initial commit

---
+++ source/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Halit Cetin (halitcetin@live.com)
+ * @brief Temaplate variables example
+ * @version 0.1
+ * @date 04-12-2022
+ * 
+ * @copyright Copyright (c) 2022
+ */
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "selam" << std::endl;
+    printf("deneme");
+    return 0;
+}
